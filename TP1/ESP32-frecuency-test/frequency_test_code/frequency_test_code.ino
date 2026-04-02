cd2ee9cbc1a5982fc359d189ba97cd68802029f6
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2026-04-02 20:32:14 -0300

    fix: improve output formatting and variable type for integer sum

--- TP1/ESP32-frecuency-test/frequency_test_code/frequency_test_code.ino
+++ TP1/ESP32-frecuency-test/frequency_test_code/frequency_test_code.ino
@@ -5,20 +5,20 @@ void setup()
 {
   Serial.begin(115200);
 
+  delay(3000);
+
   int frequency = 240;
-
   setCpuFrequencyMhz(frequency);
-
-  delay(2000); // tiempo para abrir el monitor serie
 
   Serial.println("Inicio prueba...");
   Serial.print("Frecuencia seteada a: ");
   Serial.print(frequency);
-  Serial.println("MHz");
+  Serial.println(" MHz");
 
   // -------- INT --------
   Serial.println("Calculando suma de enteros...");
-  volatile int sumaInt = 0;
+  volatile int32_t sumaInt = 0;
+
   startTime = millis();
 
   for (long i = 0; i < 50000000; i++)
@@ -29,11 +29,12 @@ void setup()
   endTime = millis();
   Serial.print("Tiempo enteros: ");
   Serial.print(endTime - startTime);
-  Serial.println(" milisegundos");
+  Serial.println(" ms");
 
   // -------- FLOAT --------
   Serial.println("Calculando suma de flotantes...");
   volatile float sumaFloat = 0;
+
   startTime = millis();
 
   for (long i = 0; i < 50000000; i++)
@@ -44,7 +45,7 @@ void setup()
   endTime = millis();
   Serial.print("Tiempo float: ");
   Serial.print(endTime - startTime);
-  Serial.println(" milisegundos");
+  Serial.println(" ms");
 }
 
 void loop() {}
