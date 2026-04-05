48d1bc44e99b64ca630cc1c54fcbb680d0272cb4
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2026-04-05 12:13:10 -0300

    fix: refactor main function to use read_data for file input

--- TP2/c/main_i2.c
+++ TP2/c/main_i2.c
@@ -1,25 +1,28 @@
-// c/main.c
+// c/main_i2.c
 
+#include "file_reader.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 extern int process_value_asm(float value);
 
-int main()
-{
-    printf("C layer OK\n");
-    FILE *file = fopen("data/gini_values.txt", "r");
-    if (!file) {
-        perror("Error abriendo el archivo");
-        return 1;
-    }
+int main() {
+  int size;
+  printf("C layer OK\n");
 
-    float value;
-    while (fscanf(file, "%f", &value) == 1) {
-        int result = process_value_asm(value);
-        printf("%d\n", result);
-    }
+  float *data = read_data("data/gini_values.txt", &size);
+  if (!data) {
+    perror("Error leyendo los datos");
+    return 1;
+  }
 
-    fclose(file);
-    return 0;
+  float value;
+  for (int i = 0; i < size; i++) {
+    value = data[i];
+    int result = process_value_asm(value);
+    printf("%d\n", result);
+  }
+
+  free(data);
+  return 0;
 }
