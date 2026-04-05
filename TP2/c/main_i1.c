c4a61e95b3cd266c75bc41b6a4edae761d96879a
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2026-04-05 12:12:36 -0300

    fix: remove pipeline.c as part of integration cleanup

--- TP2/c/main_i1.c
+++ TP2/c/main_i1.c
@@ -1,29 +1,29 @@
+// c/main_i1.c
+
+#include "file_reader.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "file_reader.h"
 
 // Función de procesamiento (simula ASM)
-int process_value(float value) {
-    return (int)value + 1;
-}
+int process_value(float value) { return (int)value + 1; }
 
 int main() {
-    int size;
+  int size;
 
-    float* data = read_data("data/gini_data.txt", &size);
+  float *data = read_data("data/gini_values.txt", &size);
 
-    if (data == NULL) {
-        printf("Error reading file\n");
-        return 1;
-    }
+  if (data == NULL) {
+    printf("Error reading file\n");
+    return 1;
+  }
 
-    printf("Processed values:\n");
+  printf("Processed values:\n");
 
-    for (int i = 0; i < size; i++) {
-        int result = process_value(data[i]);
-        printf("%d\n", result);
-    }
+  for (int i = 0; i < size; i++) {
+    int result = process_value(data[i]);
+    printf("%d\n", result);
+  }
 
-    free(data);
-    return 0;
+  free(data);
+  return 0;
 }
