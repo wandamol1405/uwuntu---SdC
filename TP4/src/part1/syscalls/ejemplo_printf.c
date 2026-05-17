b1f15f401981e97da8581ccef980798c55654f33
Wanda Molina <wanda.molina@mi.unc.edu.ar>
2026-05-17 15:21:28 -0300

    feat: add initial kernel module and supporting files for TP4 project

---
+++ TP4/src/part1/syscalls/ejemplo_printf.c
@@ -0,0 +1,10 @@
+/* 	gcc -Wall -o EjePrintfCall EjePrintfCall.c 
+	strace ./EjePrintfCall						*/
+#include <stdio.h>
+int main(void)
+{ 
+	printf("We can and we must undrestand computers now\n"); 
+	printf("\n"); 
+
+	return 0; 
+}
